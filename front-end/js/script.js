a68effc626647598f0f0aa35fc141a23fbd23224
Anthony de Zutter <60969070+anthonyzutter@users.noreply.github.com>
2020-08-20 21:53:17 -0300

    front end e js atualizado

--- front-end/js/script.js
+++ front-end/js/script.js
@@ -1,26 +1,31 @@
 $(function () {
 
+function listar_itens() {
     $.ajax({
         url: 'http://localhost:5000/listar_itens',
         method: 'GET',
         dataType: 'json',
         success: listar,
         error: function () {
-            alert("erro ao ler dados, verifique o backend");
+            alert("Erro ao ler dados, verifique o backend");
         }
     });
 
     function listar(itens) {
-        for (var i in comidas) {
+        $('#corpoTabelaEsportes').empty();
+        console.log(itens);
+        for (var i in itens) {
             lin = '<tr>' +
-                '<td>' + itens[i].id + '</td>' +
+                //'<td>' + itens[i].id + '</td>' +
                 '<td>' + itens[i].nome + '</td>' +
-                '<td>' + itens[i].preço + '</td>' +
+                '<td>' + itens[i].preco + '</td>' +
                 '<td>' + itens[i].exterior + '</td>' +
                 '<td>' + itens[i].quantidade + '</td>' +
                 '</tr>';
-            $('#corpoTabelaItem').append(lin);
+            $('#corpoTabelaItens').append(lin);
+        }   
         }
-    }
+    };
+    listar_itens();
 
 });
